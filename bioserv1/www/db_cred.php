c76e45f3c7ed50557ed7abd82c3ffb01cac9a1c1
Daniil <daniil@rutorika.ru>
2024-05-14 20:17:52 +0300

    Init new configuration

--- bioserv1/www/db_cred.php
+++ bioserv1/www/db_cred.php
@@ -1,8 +1,8 @@
 <?php
-    $serv = 'bio1mysql';
-    $datb = 'bioserver';
-    $user = 'bioserver';
-    $pass = 'xxxxxxxxxxxxxxxx';
+    $serv = getenv("DB_HOST");
+    $datb = getenv("DB_DATABASE");
+    $user = getenv("DB_USER");
+    $pass = getenv("DB_PASSWORD");
     
     $conn = mysqli_connect($serv, $user, $pass, $datb)
         or die ("connection error");
