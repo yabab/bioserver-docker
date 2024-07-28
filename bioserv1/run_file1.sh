0846da052c60aa2d6e985d7e0b3ab1d73685280c
Daniil <gsuper36@gmail.com>
2024-07-28 11:18:27 +0300

    add bioserv1 patch

--- bioserv1/run_file1.sh
+++ bioserv1/run_file1.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+if [ -e ./bioserver/patch.raw ]
+    then 
+        mv ./bioserver/patch.raw .
+    else 
+        echo 'patch.raw not found'
+fi
+
 java -cp lib/mysql-connector.jar:bin bioserver.ServerMain
